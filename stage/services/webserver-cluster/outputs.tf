66eb578e7b4feb2be478d1eb500bf79cd4c7d7c5
Girish Bitla <gbitla@salesforce.com>
2018-07-05 15:12:15 -0700

    mysql and webserver-cluster

---
+++ stage/services/webserver-cluster/outputs.tf
@@ -0,0 +1,20 @@
+output "launch_configuration_id" {
+  value = "${aws_launch_configuration.example.id}"
+}
+
+output "security_groups" {
+  value = "${aws_security_group.elb-sg.id}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
+
+#output "public_ip" {
+#  value = "${aws_instance.example.public_ip}"
+#}
+
+
+#output "public_dns_name" {
+#  value = "${aws_instance.example.public_dns}"
+#}
